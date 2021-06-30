ad514bc7617a2b7250edd8a623c451bba7c566a2
Bharath Kumar Hg <bharath.kumar.hg@ibm.com>
2021-06-30 11:09:00 +0530

    Created Xyz and Modified abc

--- Abc.java
+++ Abc.java
@@ -2,5 +2,8 @@ class Abc
 {
 public static void main()
 {
+ for(int i=o;i<5;i++){
+
 System.out.println("HELLO");
+}
 }}
---
+++ Xyz.java
@@ -0,0 +1,7 @@
+class Xyz
+{
+public satic void main (String args[])
+{
+System.out.println("hIIIIIIIIIII");
+}
+}
